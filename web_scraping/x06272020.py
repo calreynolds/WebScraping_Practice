3a2d12a6b2036191deaabc946cbe2b0c7e35c410
calreynolds <49540501+calreynolds@users.noreply.github.com>
2020-07-14 18:17:29 -0400

    Add files via upload

---
+++ web_scraping/x06272020.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+from BeautifulSoup import BeautifulSoup
+import pandas as pd
+
+def pingIMDB():
+	driver = webdriver.Chrome();
+
+def main():
+	pingIMDB();
+
+main();
